97bf832e72f28a49b4ce4268bb6ae8ee619c5dc5
Christopher Oliveira <chrisoliveira1787@gmail.com>
2019-04-16 12:24:35 -0400

    got weather working and no search results found for events.js

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -11,12 +11,11 @@ $(document).ready(function () {
   console.log(paramObj)
 
   // Ticketmaster requires a specific date format for their API call
-  ticketMasterDate = moment(paramObj.date, "YYYY-MM-DD").format("YYYY-MM-DDTHH:mm:ssZ");
+  var ticketMasterDate = moment(paramObj.date, "YYYY-MM-DD").format("YYYY-MM-DDTHH:mm:ssZ");
+  var ticketMasterEndDate = moment(paramObj.date, "YYYY-MM-DD").add(23, "h").format("YYYY-MM-DDTHH:mm:ssZ")
 
-  var ticketMasterUrl = `https://alex-rosencors.herokuapp.com/?url=https://app.ticketmaster.com/discovery/v2/events.json?size=3&apikey=8pz0roVaKoVrDwdaTb4ChFO20fDnHIrg&city=${paramObj.city}&stateCode=${paramObj.state}&startDateTime=${ticketMasterDate}`
-
-  // 34298613862c897b961ca0ebebbda16d
-  // var openWeatherUrl = `api.openweathermap.org/data/2.5/forecast?zip=${tmZipcode},us`
+  var ticketMasterUrl = `https://alex-rosencors.herokuapp.com/?url=https://app.ticketmaster.com/discovery/v2/events.json?size=12&apikey=8pz0roVaKoVrDwdaTb4ChFO20fDnHIrg&city=${paramObj.city}&stateCode=${paramObj.state}&startDateTime=${ticketMasterDate}&endDateTime=${ticketMasterEndDate}`
+  console.log(ticketMasterUrl);
 
   $.ajax({
       url: ticketMasterUrl,
@@ -25,143 +24,164 @@ $(document).ready(function () {
     .then(function (ticketMasterResponse) {
       console.log(ticketMasterResponse);
 
-      var tmResults = ticketMasterResponse._embedded.events;
-
-      if (ticketMasterResponse._embedded[0]) {
-        $("#event-wrapper").text("No Search Results Found");
+      var responseKeys = Object.keys(ticketMasterResponse);
+      if (!responseKeys.includes("_embedded")) {
+        console.log("Nothing found");
+        $("#event-wrapper").text("No Search Results Found. Please check your city/state again.");
         return false;
       }
 
-      // This section is on Events Page load after initial search from main page //
+      var tmResults = ticketMasterResponse._embedded.events;
 
-      tmResults.forEach(function (event) {
+        // This section is on Events Page load after initial search from main page //
 
-        var tmZipCode = event._embedded.venues[0].postalCode;
+        tmResults.forEach(function (event) {
 
-        var eventDiv = $(`<div class="card-wrapper col-12 col-md-4 mb-2">`);
-        eventDiv.attr("data-zip", tmZipCode);
-        var ticketButton = $(`<a href=${event.url} target="_blank" class="btn btn-block btn-danger">`).text("Get Tickets");
+          var tmZipCode = event._embedded.venues[0].postalCode;
+          var tmEventName = event.name;
+
+          var eventDiv = $(`<div class="card-wrapper col-12 col-md-4 mb-2">`);
+          eventDiv.attr("data-zip", tmZipCode);
+          eventDiv.attr("event-name", tmEventName);
+          var ticketButton = $(`<a href=${event.url} target="_blank" class="btn btn-block btn-danger">`).text("Get Tickets");
 
 
-        var eventImg = $(`<img class="card-img-top" src=${event.images[0].url} />`);
-        var eventDivBody = $(`<div class="card-body">`);
+          var eventImg = $(`<img class="card-img-top" src=${event.images[1].url} />`);
+          var eventDivBody = $(`<div class="card-body">`);
 
-        var eventH5 = $(`<h5 class="card-title">`);
-        eventH5.text(event.name);
-        var eventP = $(`<p class="card-text">`);
+          var eventH5 = $(`<h5 class="card-title">`);
+          eventH5.text(event.name);
+          var eventP = $(`<p class="card-text">`);
 
-        var venueName = event._embedded.venues[0].name
-        var eventDate = event.dates.start.localDate
-        var eventDateFormatted = moment(eventDate, "YYYY-MM-DDTHH:mm:ssZ").format("M-DD-YYYY");
-        var eventTime = event.dates.start.localTime
-        var eventTimeFormatted = moment(eventTime, "HH:mm:ss").format("h:mm a");
-        eventP.append(`${venueName}<br>${eventDateFormatted}<br>${eventTimeFormatted}`);
-        eventDivBody.append(eventH5, eventP, ticketButton);
-        eventDiv.append(eventImg, eventDivBody);
+          var venueName = event._embedded.venues[0].name
+          var eventDate = event.dates.start.localDate
+          var eventDateFormatted = moment(eventDate, "YYYY-MM-DDTHH:mm:ssZ").format("M-DD-YYYY");
+          var eventTime = event.dates.start.localTime
+          var eventTimeFormatted = moment(eventTime, "HH:mm:ss").format("h:mm a");
+          eventP.append(`${venueName}<br>${eventDateFormatted}<br>${eventTimeFormatted}`);
+          eventDivBody.append(eventH5, eventP, ticketButton);
+          eventDiv.append(eventImg, eventDivBody);
 
-        $("#destination").text(paramObj.city);
-        // $("#date").text(eventDateFormatted);
-        $("#event-wrapper").append(eventDiv);
+          $("#destination").text(paramObj.city);
+          // $("#date").text(eventDateFormatted);
+          $("#event-wrapper").append(eventDiv);
 
-      });
+        });
+        // End initial search section //
 
-      // End initial search section //
-      $("#event-wrapper").on("click", ".card-wrapper", function() {
-        var tmZipCode = $(this).attr("data-zip");
-        // 34298613862c897b961ca0ebebbda16d
-        var openWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${tmZipCode}&APPID=34298613862c897b961ca0ebebbda16d`
 
-        $.ajax({
-          url: openWeatherUrl,
-          method: "GET"
+        $("#event-wrapper").on("click", ".card-wrapper", function () {
+          var tmZipCode = $(this).attr("data-zip");
+          var tmEventName = $(this).attr("event-name");
+          // 34298613862c897b961ca0ebebbda16d
+          var openWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${tmZipCode}&APPID=34298613862c897b961ca0ebebbda16d`
+
+          $.ajax({
+              url: openWeatherUrl,
+              method: "GET"
+            })
+            .then(function (openWeatherResponse) {
+              console.log(openWeatherResponse);
+
+              var owResults = openWeatherResponse.list[0];
+              var owTempMax = owResults.main.temp_max;
+              var owTempMin = owResults.main.temp_min;
+              var tempMaxConverted = parseInt(1.8 * (owTempMax - 273) + 32);
+              var tempMinConverted = parseInt(1.8 * (owTempMin - 273) + 32);
+              var weatherCond = owResults.weather[0].main;
+              console.log(weatherCond);
+
+              var weatherEventName = $("<p>").append(tmEventName);
+              var conditionPTag = $("<p>").append(weatherCond);
+              var temperaturePTag = $("<p>").append(`Low: ${tempMinConverted}° - High: ${tempMaxConverted}°`);
+
+
+              var weatherDivBody = $(`<div class="card-body">`);
+
+              weatherDivBody.append(weatherEventName);
+              if (weatherCond === "Rain") {
+                weatherDivBody.append($("<img src='assets/images/rain.png' alt='rain' class='img-fluid' />"));
+              } else if (weatherCond === "Snow") {
+                weatherDivBody.append($("<img src='assets/images/snow.png' alt='rain' class='img-fluid' />"));
+              } else if (weatherCond === "Clouds") {
+                weatherDivBody.append($("<img src='assets/images/cloudy.png' alt='rain' class='img-fluid' />"));
+              } else if (weatherCond === "Clear") {
+                weatherDivBody.append($("<img src='assets/images/clear-sky.png' alt='rain' class='img-fluid' />"));
+              } else if (weatherCond === "Wind") {
+                weatherDivBody.append($("<img src='assets/images/windy.png' alt='rain' class='img-fluid' />"));
+              };
+              weatherDivBody.append(conditionPTag, temperaturePTag);
+
+              $("#weather-wrapper").html(weatherDivBody);
+            })
         })
-        .then(function (openWeatherResponse) {
-          console.log(openWeatherResponse);
-
-          var owResults = openWeatherResponse.list[0];
-          var owTempMax = owResults.main.temp_max;
-          var owTempMin = owResults.main.temp_min;
-          var tempMaxConverted = parseInt(1.8 * (owTempMax - 273) + 32);
-          var tempMinConverted = parseInt(1.8 * (owTempMin - 273) + 32);
-          var weatherCond = owResults.weather[0].description;
-          var weatherIcon = owResults.weather[0].icon;
-
-          var iconPTag = $("<p>").append(weatherIcon);
-          var conditionPTag = $("<p>").append(weatherCond);
-          var lowTemperaturePTag = $("<p>").append(`Low: ${tempMinConverted}°`);
-          var highTemperaturePTag = $("<p>").append(`High: ${tempMaxConverted}°`)
 
 
-          var weatherDivBody = $(`<div class="card-body">`);
-          weatherDivBody.append(iconPTag, conditionPTag, lowTemperaturePTag, highTemperaturePTag);
+        // Begin section for the events page specific search bar //
 
-          $("#weather-wrapper").html(weatherDivBody);
-        })
-      })
+        $("#searchBtn").on("click", function (event) {
+          event.preventDefault();
+
+          // read from user input tags
+
+          var userInput = {
+            city: $("#city-id").val().trim(),
+            state: $("#state-id").val(),
+            date: $("#date-id").val().trim()
+          };
+
+          var eventUrl = "events.html?city=" + userInput.city + "&state=" + userInput.state + "&date=" + userInput.date;
+
+          location.href = eventUrl;
+
+          $.ajax({
+              url: ticketMasterUrl,
+              method: "GET"
+            })
+            .then(function (ticketMasterResponse) {
+
+              var tmResults = ticketMasterResponse._embedded.events
+
+              if (ticketMasterResponse.page.totalElements === 0) {
+                $("#event-wrapper").text("No Search Results Found. Please check your city/state again.");
+                console.log(false);
+              } else {
+
+                tmResults.forEach(function (event) {
+
+                  var tmZipCode = event._embedded.venues[0].postalCode;
+
+                  var eventDiv = $(`<div class="card-wrapper col-12 col-md-4 mb-2">`);
+                  eventDiv.attr(`${tmZipCode}`);
+
+                  var ticketButton = $(`<a class="btn btn-block btn-danger">`).text("Get Tickets");
+
+                  var eventImg = $(`<img class="card-img-top" src=${event.images[1].url}/>`);
+                  var eventDivBody = $(`<div class="card-body">`);
+
+                  var eventH5 = $(`<h5 class="card-title">`);
+                  eventH5.text(event.name);
+                  var eventP = $(`<p class="card-text">`);
+                  var ticketButton = $(`<button class="btn btn-block btn-danger">`).text("Get Tickets!");
+                  var venueName = event._embedded.venues[0].name
+                  var eventDate = event.dates.start.localDate
+                  var eventDateFormatted = moment(eventDate, "YYYY-MM-DDTHH:mm:ssZ").format("M-DD-YYYY");
+                  var eventTime = event.dates.start.localTime
+                  var eventTimeFormatted = moment(eventTime, "HH:mm:ss").format("h:mm a");
+                  eventP.append(`${venueName}<br>${eventDateFormatted}<br>${eventTimeFormatted}`);
+                  eventDivBody.append(eventH5, eventP, ticketButton);
+                  eventDiv.append(eventImg, eventDivBody);
+
+                  $("#destination").text(paramObj.city);
+                  $("#date").text(eventDateFormatted);
+                  $("#event-wrapper").append(eventDiv);
 
 
-      // Begin section for the events page specific search bar //
-
-      $("#searchBtn").on("click", function (event) {
-        event.preventDefault();
-
-        // read from user input tags
-
-        var userInput = {
-          city: $("#city-id").val().trim(),
-          state: $("#state-id").val(),
-          date: $("#date-id").val().trim()
-        };
-
-        var eventUrl = "events.html?city=" + userInput.city + "&state=" + userInput.state + "&date=" + userInput.date;
-
-        $.ajax({
-            url: ticketMasterUrl,
-            method: "GET"
-          })
-          .then(function (ticketMasterResponse) {
-
-            if (ticketMasterResponse._embedded[0]) {
-              $("#event-wrapper").text("No Search Results Found");
-              return false;
-            }
-
-            var tmResults = ticketMasterResponse._embedded.events
-
-            tmResults.forEach(function (event) {
-
-              var tmZipCode = event._embedded.venues[0].postalCode;
-
-              var eventDiv = $(`<div class="card-wrapper col-12 col-md-4 mb-2">`);
-              eventDiv.attr(`${tmZipCode}`);
-
-              var ticketButton = $(`<a class="btn btn-block btn-danger">`).text("Get Tickets");              
-
-              var eventImg = $(`<img class="card-img-top" src=${event.images[0].url}/>`);
-              var eventDivBody = $(`<div class="card-body">`);
-
-              var eventH5 = $(`<h5 class="card-title">`);
-              eventH5.text(event.name);
-              var eventP = $(`<p class="card-text">`);
-              var ticketButton = $(`<button class="btn btn-block btn-danger">`).text("Get Tickets!");
-              var venueName = event._embedded.venues[0].name
-              var eventDate = event.dates.start.localDate
-              var eventDateFormatted = moment(eventDate, "YYYY-MM-DDTHH:mm:ssZ").format("M-DD-YYYY");
-              var eventTime = event.dates.start.localTime
-              var eventTimeFormatted = moment(eventTime, "HH:mm:ss").format("h:mm a");
-              eventP.append(`${venueName}<br>${eventDateFormatted}<br>${eventTimeFormatted}`);
-              eventDivBody.append(eventH5, eventP, ticketButton);
-              eventDiv.append(eventImg, eventDivBody);
-
-              $("#destination").text(paramObj.city);
-              $("#date").text(eventDateFormatted);
-              $("#event-wrapper").append(eventDiv);
-
-              location.href = eventUrl;
-
+                });
+              }
             });
-          });
-      });
+        });
+      
     });
   //slideshow jumbotron
   var backgroundImg = ["assets/images/events-hero2.jpg", "assets/images/events-hero3.jpg", "assets/images/events-hero4.jpg, assets/images/events-hero5.jpg, assets/images/events-hero6.jpg"];
